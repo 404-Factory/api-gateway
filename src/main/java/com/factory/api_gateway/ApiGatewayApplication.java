f812950c3a03244141ef90d3b8d151689942bcbc
rlarkdls123 <lina083017@gmail.com>
2026-05-28 15:41:04 +0900

    포트 8081로 gateway 작동 확인

--- src/main/java/com/factory/api_gateway/ApiGatewayApplication.java
+++ src/main/java/com/factory/api_gateway/ApiGatewayApplication.java
@@ -2,30 +2,12 @@ package com.factory.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-
-import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableFeignClients
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        loadDotenv();
         SpringApplication.run(ApiGatewayApplication.class, args);
-    }
-
-    private static void loadDotenv() {
-        Dotenv dotenv = Dotenv.configure()
-                .ignoreIfMalformed()
-                .ignoreIfMissing()
-                .load();
-
-        dotenv.entries().forEach(entry -> {
-            String key = entry.getKey();
-            if (System.getProperty(key) == null && System.getenv(key) == null) {
-                System.setProperty(key, entry.getValue());
-            }
-        });
     }
 }
--- src/main/java/com/factory/api_gateway/config/CorsConfig.java
+++ src/main/java/com/factory/api_gateway/config/CorsConfig.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
@@ -31,6 +31,7 @@ public class CorsConfig {
         config.setAllowCredentials(true);
         config.setMaxAge(3600L);
 
+        // 리액티브용 UrlBasedCorsConfigurationSource 객체 생성
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return source;
